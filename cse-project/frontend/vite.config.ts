9207b71fc4d6ef5917b912ef44ddd922fa066b4c
{yuta-kintone} <{yuta@kintone.com}>
2024-11-05 01:06:53 -0700

    Update frontend port in vite.config.ts

--- cse-project/frontend/vite.config.ts
+++ cse-project/frontend/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  server: {
+    port: 3000, // ここでポートを指定
+  },
   plugins: [react()],
   resolve: {
     alias: {
